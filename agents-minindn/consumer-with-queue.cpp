3d9891df51c2cc6f83a2f6c0428a211b70e916c0
Andre Dexheimer Carneiro <andre.dxc@hotmail.com>
2021-04-24 19:31:37 -0300

    asdf

--- agents-minindn/consumer-with-queue.cpp
+++ agents-minindn/consumer-with-queue.cpp
@@ -7,14 +7,14 @@
 *
 */
 #include <ndn-cxx/face.hpp>
+#include <ndn-cxx/util/scheduler.hpp>
+#include <boost/asio/io_service.hpp>
 #include <iostream>
 #include <chrono>
 #include <ctime>
 #include <libgen.h>
-#include <thread>
 #include <stdio.h>
 
-#define N_DEFAULT_PAYLOAD_BYTES 8000
 #define N_MAX_PACKET_BYTES      8000
 
 typedef struct {
@@ -34,25 +34,40 @@ namespace examples {
 class Consumer
 {
    public:
+      Consumer();
       void run(std::string strNode, std::string strTimestamp, std::string strQueueFileName);
 
    private:
-      void onData(const Interest&, const Data& data)       const;
-      void onNack(const Interest&, const lp::Nack& nack)   const;
-      void onTimeout(const Interest& interest)             const;
-      void logResult(float sTimeDiff, const char* pResult, std::string strInterest, std::string strTimestamp) const;
-      void logResultWithSize(float sTimeDiff, const char* pResult, std::string strInterest, std::string strTimestamp, size_t nSize) const;
-      void consumePacket(std::string strInterest);
+      void delayedInterest(C2_DATA dataBuff);
       std::vector<C2_DATA> readDataQueue(std::string strHostName, std::string strFilePath);
 
+      void onData(const Interest&, const Data& data, const std::chrono::steady_clock::time_point& dtBegin)     const;
+      void onNack(const Interest&, const lp::Nack& nack, const std::chrono::steady_clock::time_point& dtBegin) const;
+      void onTimeout(const Interest& interest, const std::chrono::steady_clock::time_point& dtBegin)           const;
+
+      void logResult(float sTimeDiff, const char* pResult, std::string strInterest, std::string strTimestamp, size_t nSize) const;
+
    private:
+      // Explicitly create io_service object, which will be shared between Face and Scheduler
+      boost::asio::io_service m_ioService;
+      Scheduler m_scheduler;
+
       Face m_face;
       std::string m_strHostName;
-      std::string m_strInterest;
       std::string m_strLogPath;
       std::string m_strTimestamp;
-      std::chrono::steady_clock::time_point m_dtBegin;
 };
+
+// --------------------------------------------------------------------------------
+//   Constructor
+//
+//
+// --------------------------------------------------------------------------------
+Consumer::Consumer()
+   : m_face(m_ioService) // Create face with io_service object
+   , m_scheduler(m_ioService)
+   {
+   }
 
 // --------------------------------------------------------------------------------
 //   run
@@ -69,44 +84,80 @@ void Consumer::run(std::string strNode, std::string strTimestamp, std::string st
    // Read and validate input parameters
    m_strHostName  = strNode;
    m_strTimestamp = strTimestamp;
+
+   if (m_strHostName.length() > 0)
+      m_strLogPath = "/tmp/minindn/" + m_strHostName + "/consumer.log";
+   else
+      m_strLogPath = "/tmp/minindn/consumer.log";
    
    fprintf(stdout, "[Consumer::run] Running consumer with HostName=%s; QueueFile=%s\n", m_strHostName.c_str(), strQueueFileName.c_str());
-   
+
    lstData = readDataQueue(strNode, strQueueFileName);
+   fprintf(stdout, "[Consumer::run] Read a total of %d data packages from file=%s\n", (int) lstData.size(), strQueueFileName.c_str());
+
+   ///////////////////////////////////////////
+   // Scheadule data to be consumed
    for (i = 0; i < lstData.size(); i++){
       dataBuff = lstData[i];
-      fprintf(stdout, "[Consumer::run] %d, Type=%d, ID=%d, Payload=%d, Prod=%s, Cons=%s\n", dataBuff.nTimeMs, dataBuff.nType, dataBuff.nId, dataBuff.nPayload, dataBuff.strProd, dataBuff.strCons);
+      fprintf(stdout, "[Consumer::run] Scheadule %d, Type=%d, ID=%d, Payload=%d, Prod=%s, Cons=%s\n", dataBuff.nTimeMs, dataBuff.nType, dataBuff.nId, dataBuff.nPayload, dataBuff.strProd, dataBuff.strCons);
+      m_scheduler.schedule(boost::chrono::milliseconds(dataBuff.nTimeMs), [this] { delayedInterest(dataBuff); });
    }
-
-   fprintf(stdout, "[Consumer::run] Read a total of %d data packages\n", lstData.size());
+   
+   // m_ioService.run() will block until all events finished or m_ioService.stop() is called
+   m_ioService.run();
    return;
 }
 
 // --------------------------------------------------------------------------------
-//  consumePacket
+//   delayedInterest
 //
 //
 // --------------------------------------------------------------------------------
-void Consumer::consumePacket(std::string strInterest){
-   Face face;
+void Consumer::delayedInterest(C2_DATA dataBuff){
+
+   char strBuf[100], strPrefix[60];
+   int i, nPackets, nPacketPayload;
+   bool bHasLeftover;
    Name interestName;
    Interest interest;
 
-   ///////////////////////////////////////////////
-   // Configure interest
-   interestName = Name(strInterest);
-   interest     = Interest(interestName);
-   interest.setCanBePrefix(false);
-   interest.setMustBeFresh(true);
-   interest.setInterestLifetime(6_s); // The default is 4 seconds
+   snprintf(strPrefix, sizeof(strPrefix), "/ndn/%s-site/%s/C2Data-%d-Type%d", dataBuff.strProd, dataBuff.strProd, dataBuff.nId, dataBuff.nType);
 
-   fprintf(stdout, "[Consumer::run] Sending interest=%s\n", strInterest.c_str());
+   // Determine number of packets necessary to fulfill the interest
+   nPackets     = dataBuff.nPayload / N_MAX_PACKET_BYTES;
+   bHasLeftover = false;
+   if ((dataBuff.nPayload % N_MAX_PACKET_BYTES) > 0){
+      bHasLeftover = true;
+      nPackets++;
+   }
 
-   face.expressInterest(interest, bind(&Consumer::onData, this,   _1, _2),
-      bind(&Consumer::onNack, this, _1, _2), bind(&Consumer::onTimeout, this, _1));
+   // Express interest for all packets
+   for (i = 0; i < nPackets; i++){
 
-   // pocessEvents will block until the requested data is received or a timeout occurs
-   face.processEvents();
+      if ((bHasLeftover) && (i+1 == nPackets)){
+         // Last packet
+         nPacketPayload = dataBuff.nPayload % N_MAX_PACKET_BYTES;
+      }
+      else{
+         // Any other packet
+         nPacketPayload = N_MAX_PACKET_BYTES;
+      }
+
+      fprintf(stdout, "[Consumer::run] Expressing %d/%d for interest=%s\n", i+1, nPackets, strBuf);
+      snprintf(strBuf, sizeof(strBuf), "%s-%db-%dof%d", strPrefix, nPacketPayload, i+1, nPackets);
+
+      dtBegin = std::chrono::steady_clock::now();
+      interestName(strBuf);
+      interest(interestName);
+      interest.setCanBePrefix(false);
+      interest.setMustBeFresh(true);
+      interest.setInterestLifetime(6_s);
+
+      m_face.expressInterest(interest,
+                           bind(&Consumer::onData, this, _1, _2, dtBegin),
+                           bind(&Consumer::onNack, this, _1, _2, dtBegin),
+                           bind(&Consumer::onTimeout, this, _1, dtBegin));
+   }
 }
 
 // --------------------------------------------------------------------------------
@@ -114,16 +165,15 @@ void Consumer::consumePacket(std::string strInterest){
 //
 //
 // --------------------------------------------------------------------------------
-void Consumer::onData(const Interest& interest, const Data& data) const
+void onData(const Interest& interest, const Data& data, const std::chrono::steady_clock::time_point& dtBegin) const
 {
    float sTimeDiff;
    std::chrono::steady_clock::time_point dtEnd;
 
    dtEnd     = std::chrono::steady_clock::now();
-   sTimeDiff = std::chrono::duration_cast<std::chrono::microseconds>(dtEnd - m_dtBegin).count();
+   sTimeDiff = std::chrono::duration_cast<std::chrono::microseconds>(dtEnd - dtBegin).count();
 
-   // logResult(sTimeDiff, "DATA", m_strTimestamp);
-   logResultWithSize(sTimeDiff, "DATA", interest.getName().toUri(), m_strTimestamp, data.getContent().value_size());
+   logResult(sTimeDiff, "DATA", interest.getName().toUri(), m_strTimestamp, data.getContent().value_size());
 
    std::cout << "[Consumer::onData] Received Data=\n" << data << "Delay=" << sTimeDiff << "; Size=" << data.getContent().value_size() << std::endl;
 }
@@ -133,17 +183,17 @@ void Consumer::onData(const Interest& interest, const Data& data) const
 //
 //
 // --------------------------------------------------------------------------------
-void Consumer::onNack(const Interest& interest, const lp::Nack& nack) const
+void Consumer::onNack(const Interest& interest, const lp::Nack& nack, const std::chrono::steady_clock::time_point& dtBegin) const
 {
    float sTimeDiff;
    std::chrono::steady_clock::time_point dtEnd;
 
    dtEnd     = std::chrono::steady_clock::now();
-   sTimeDiff = std::chrono::duration_cast<std::chrono::microseconds>(dtEnd - m_dtBegin).count();
+   sTimeDiff = std::chrono::duration_cast<std::chrono::microseconds>(dtEnd - dtBegin).count();
 
-   logResult(sTimeDiff, "NACK", interest.getName().toUri(), m_strTimestamp);
+   logResult(sTimeDiff, "NACK", interest.getName().toUri(), m_strTimestamp, 0);
 
-   std::cout << "[Consumer::onNack] Received Nack interest=" << m_strInterest <<
+   std::cout << "[Consumer::onNack] Received Nack interest=" << interest.getName().toUri() <<
       ";Reason=" << nack.getReason() << "Delay=" << sTimeDiff << std::endl;
 }
 
@@ -152,7 +202,7 @@ void Consumer::onNack(const Interest& interest, const lp::Nack& nack) const
 //
 //
 // --------------------------------------------------------------------------------
-void Consumer::onTimeout(const Interest& interest) const
+void Consumer::onTimeout(const Interest& interest, const std::chrono::steady_clock::time_point& dtBegin) const
 {
    float sTimeDiff;
    std::chrono::steady_clock::time_point dtEnd;
@@ -160,11 +210,11 @@ void Consumer::onTimeout(const Interest& interest) const
    std::cout << "[Consumer::onTimeout] Timeout for " << interest << std::endl;
 
    dtEnd     = std::chrono::steady_clock::now();
-   sTimeDiff = std::chrono::duration_cast<std::chrono::microseconds>(dtEnd - m_dtBegin).count();
+   sTimeDiff = std::chrono::duration_cast<std::chrono::microseconds>(dtEnd - dtBegin).count();
 
-   logResult(sTimeDiff, "TIMEOUT", interest.getName().toUri(), m_strTimestamp);
+   logResult(sTimeDiff, "TIMEOUT", interest.getName().toUri(), m_strTimestamp, 0);
 
-   std::cout << "[Consumer::onTimeout] Timeout for interest=" << m_strInterest << "Delay="
+   std::cout << "[Consumer::onTimeout] Timeout for interest=" << interest.getName().toUri() << "Delay="
       << sTimeDiff << std::endl;
 }
 
@@ -176,7 +226,6 @@ void Consumer::onTimeout(const Interest& interest) const
 std::vector<C2_DATA> Consumer::readDataQueue(std::string strHostName, std::string strFilePath)
 {
     FILE* pFile;
-    char  pLineBuff[1000];
     std::vector<C2_DATA> lstData;
     C2_DATA dataBuff;
     int nLine, nRead;
@@ -185,14 +234,12 @@ std::vector<C2_DATA> Consumer::readDataQueue(std::string strHostName, std::strin
     if (pFile != NULL) {
         nLine = 0;
         nRead = 0;
-        // while (fgets(pLineBuff, sizeof(pLineBuff), pFile) != NULL){
         while (nRead != -1){
             // Format '%d;Type=%d;Id=%d;Payload=%d;Prod=%s;Cons=%s'
             // %s reads space delimited strings, so it had to be replaced with %[^;]
-            // fprintf(stdout, "Read line=%s", pLineBuff);
             nLine++;
             nRead = fscanf(pFile, "%d;Type=%d;Id=%d;Payload=%d;Prod=%[^;];Cons=%s", &dataBuff.nTimeMs, &dataBuff.nType, &dataBuff.nId, &dataBuff.nPayload, dataBuff.strProd, dataBuff.strCons);
-            if ((nRead == 6) && (strcmp(strHostName.c_str(), dataBuff.strCons) == 0)){
+            if ((nRead == 6) && (strcmp(strHostName.c_str(), dataBuff.strCons) == 0) && (strcmp(strHostName.c_str(), dataBuff.strProd) != 0)){
                 // Success, add to queue
                 lstData.push_back(dataBuff);
             }
@@ -211,31 +258,7 @@ std::vector<C2_DATA> Consumer::readDataQueue(std::string strHostName, std::strin
 //
 //
 // --------------------------------------------------------------------------------
-void Consumer::logResult(float sTimeDiff, const char* pResult, std::string strInterest, std::string strTimestamp) const
-{
-   FILE* pFile;
-
-   if (m_strHostName.length() > 0){
-      // Write results to files
-      pFile = fopen(m_strLogPath.c_str(), "a");
-
-      if (pFile){
-         fprintf(pFile, "%s;%.4f;%s;%s\n", strInterest.c_str(), sTimeDiff, pResult, strTimestamp.c_str());
-         fclose(pFile);
-      }
-      else{
-         std::cout << "[Consumer::log] ERROR opening output file for pResult=" << pResult
-             << std::endl;
-      }
-   }
-}
-
-// --------------------------------------------------------------------------------
-//   logResult
-//
-//
-// --------------------------------------------------------------------------------
-void Consumer::logResultWithSize(float sTimeDiff, const char* pResult, std::string strInterest, std::string strTimestamp, size_t nSize) const
+void Consumer::logResult(float sTimeDiff, const char* pResult, std::string strInterest, std::string strTimestamp, size_t nSize) const
 {
    FILE* pFile;
 
